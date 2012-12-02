737384ed2f706a4a75772c6a917d43b69371773e
DvTonder <david.vantonder@gmail.com>
2012-12-02 09:16:34 -0500

    Messaging: Prevent the light from being dismissed when a QM Popup wakes the device
    Change-Id: I6390a7ff0fcef7e32246ee0a75e59aa46941deaf

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -985,8 +985,8 @@ public class MessagingNotification {
             }
         }
 
+        // Set light defaults
         defaults |= Notification.DEFAULT_LIGHTS;
-
         noti.setDefaults(defaults);
 
         // set up delete intent
@@ -1131,17 +1131,21 @@ public class MessagingNotification {
             }
         }
 
-        // Post the notification
-        nm.notify(NOTIFICATION_ID, notification);
-
         // Trigger the QuickMessage pop-up activity if enabled
         // But don't show the QuickMessage if the user is in a call or the phone is ringing
         if (qmPopupEnabled && qmIntent != null) {
             TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
             if (tm.getCallState() == TelephonyManager.CALL_STATE_IDLE && !ConversationList.mIsRunning && !ComposeMessageActivity.mIsRunning) {
+                // Since a QM Popup may wake and unlock we need to prevent the light from being dismissed
+                notification.flags |= Notification.FLAG_FORCE_LED_SCREEN_OFF;
+
+                // Show the popup
                 context.startActivity(qmIntent);
             }
         }
+
+        // Post the notification
+        nm.notify(NOTIFICATION_ID, notification);
     }
 
     protected static CharSequence buildTickerMessage(

53a84b2a5d6f8983bff58b1bdf5e8a9f14c5cd47
Roger Chen <cxr514033970@gmail.com>
2013-05-03 17:01:14 +0800

    Check if mmp's encoded subject is null before used
    From AOSP:
      https://android-review.googlesource.com/#/c/57709/1
    
    Change-Id: I1de3c83b2b68128f725f8bbaf3d6764b2bdf836b
    Signed-off-by: Roger Chen <cxr514033970@gmail.com>

--- src/com/android/mms/ui/MessageUtils.java
+++ src/com/android/mms/ui/MessageUtils.java
@@ -403,7 +403,7 @@ public class MessageUtils {
     }
 
     public static int getAttachmentType(SlideshowModel model, MultimediaMessagePdu mmp) {
-        if (model == null) {
+        if (model == null || mmp == null) {
             return MessageItem.ATTACHMENT_TYPE_NOT_LOADED;
         }
 
@@ -434,10 +434,9 @@ public class MessageUtils {
             }
 
             // Handle the multimedia message only has subject
-            if (mmp != null) {
-                if (!TextUtils.isEmpty(mmp.getSubject().getString())) {
-                    return WorkingMessage.TEXT;
-                }
+            String subject = mmp.getSubject() != null ? mmp.getSubject().getString() : null;
+            if (!TextUtils.isEmpty(subject)) {
+                return WorkingMessage.TEXT;
             }
         }
 

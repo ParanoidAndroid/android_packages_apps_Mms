616f50dded9f0677db675617443f96b37fb85965
Beena More <bmore@codeaurora.org>
2013-05-24 12:36:04 -0700

    Mms: Handle Disk Storage Full Crash
    - When a device is out of disk storage space, the messages apk will crash
    and become unresponsive
    - This changes now handles the SQLiteFullException thrown by
    the app and displays a toast to the user to indicate that the
    storage is full.
    CRs-Fixed: 487749
    
    CM changes:
    - Remove printStackTrace while it is redirected to logcat and not to console on Android
    - Internationalize the error msg
    - Only return null if an error occurs
    
    Change-Id: I89956d06e5fdd008a31802c10f33364eb327dff7
    
    Conflicts:
    	res/values/cm_strings.xml

--- src/com/android/mms/data/Conversation.java
+++ src/com/android/mms/data/Conversation.java
@@ -14,8 +14,10 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SqliteWrapper;
+import android.database.sqlite.SQLiteFullException;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.provider.BaseColumns;
 import android.provider.Telephony.Mms;
 import android.provider.Telephony.MmsSms;
@@ -38,6 +40,7 @@ import com.android.mms.util.AddressUtils;
 import com.android.mms.util.DraftCache;
 
 import com.google.android.mms.pdu.PduHeaders;
+import android.widget.Toast;
 
 /**
  * An interface for finding information about conversations and/or creating new ones.
@@ -101,6 +104,8 @@ public class Conversation {
     private static Object sDeletingThreadsLock = new Object();
     private boolean mMarkAsReadBlocked;
     private boolean mMarkAsReadWaiting;
+
+    private static Handler sToastHandler = new Handler();
 
     private Conversation(Context context) {
         mContext = context;
@@ -391,8 +396,14 @@ public class Conversation {
                         sendReadReport(mContext, mThreadId, PduHeaders.READ_STATUS_READ);
                         LogTag.debug("markAsRead: update read/seen for thread uri: " +
                                 threadUri);
-                        mContext.getContentResolver().update(threadUri, sReadContentValues,
-                                UNREAD_SELECTION, null);
+                        try {
+                            mContext.getContentResolver().update(threadUri,
+                                    sReadContentValues, UNREAD_SELECTION, null);
+                        } catch (SQLiteFullException e) {
+                            Log.e(TAG, "Database is full", e);
+                            showStorageFullToast(mContext);
+                            return null;
+                        }
                     }
                     setHasUnreadMessages(false);
                 }
@@ -1155,10 +1166,23 @@ public class Conversation {
         ContentValues values = new ContentValues(1);
         values.put("seen", 1);
 
-        resolver.update(Sms.Inbox.CONTENT_URI,
-                values,
-                "seen=0",
-                null);
+        try {
+            resolver.update(Sms.Inbox.CONTENT_URI, values, "seen=0", null);
+        } catch (SQLiteFullException e) {
+            Log.e(TAG, "Database is full", e);
+            showStorageFullToast(context);
+        }
+    }
+
+    private static void showStorageFullToast(final Context context) {
+        sToastHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                final CharSequence text = context.getString(R.string.storage_is_full);
+                final int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(context, text, duration).show();
+            }
+        });
     }
 
     private static void blockingMarkAllMmsMessagesAsSeen(final Context context) {
@@ -1190,10 +1214,12 @@ public class Conversation {
         ContentValues values = new ContentValues(1);
         values.put("seen", 1);
 
-        resolver.update(Mms.Inbox.CONTENT_URI,
-                values,
-                "seen=0",
-                null);
+        try {
+            resolver.update(Mms.Inbox.CONTENT_URI, values, "seen=0", null);
+        } catch (SQLiteFullException e) {
+            Log.e(TAG, "Database is full", e);
+            showStorageFullToast(context);
+        }
 
     }
 
